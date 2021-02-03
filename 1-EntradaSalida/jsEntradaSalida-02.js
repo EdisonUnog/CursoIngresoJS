f6f5a7b721567d08d71d566eb81893f295e0b221
EDISON <edisoncmac@gmail.com>
2021-02-03 01:36:29 -0300

    nuevo

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,9 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let respuesta = prompt("esciba su nombre");
+	alert("¡Hola "+respuesta+" bienvenido!");
+
+	
 }
 
